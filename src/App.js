83e0204793a0112f4f70374b437fdde556a487f7
naoglo <glovernaomi@gmail.com>
2023-09-28 16:05:45 +0100

    News API added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './Components/SearchBar';
 import NewsStories from './Components/NewsStories';
 import SideBar from './Components/SideBar';
 import Footer from './Components/Footer';
+
+
+
 
 function App() {
   
--- src/Components/NewsStories.js
+++ src/Components/NewsStories.js
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import App from '../App';
+import axios from 'axios';
+import { Button } from 'antd';
+import { Card } from 'antd';
+//import 'antd/dist/antd.css'
 
-const fakeStories = ['Freddy Starr Ate My Hamster.', 'Underwear bandit caught, admits brief crime spree.', 
-'Flying saucer seen in Woodbridge.', 'Heatwave coming to Suffolk' ];
+const {Meta} = Card;
+
+//const fakeStories = ['Freddy Starr Ate My Hamster.', 'Underwear bandit caught, admits brief crime spree.','Flying saucer seen in Woodbridge.', 'Heatwave coming to Suffolk' ];
 
 const NewsStories = () => {
     const styles = {
@@ -11,17 +16,41 @@ const NewsStories = () => {
             backgroundColor: 'lightblue',
     },
 };
-const [story, setStory] = useState([]);
+    const [news, setNews] = useState([]);
+
+    useEffect(() =>{
+        const loadNews = async() => {
+            const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c18e89f93021422e8cb28ee5e157580f')
+            setNews(response.data.articles);
+        }
+        loadNews();
+    }, []);
+
+    console.log('news', news);
 
     return (
-        <>
-        <div className='main' style={styles.main}> 
+        <div>
+      {/*  <div className='main' style={styles.main}> 
         {fakeStories.map((fake) => <p>{fake}</p>)
 
-         }
+         } */}
+       {news && news.map((item, index) =>{
+        return (
+            <Card
+            key={index}
+            hoverable
+            style={{width: '70%'}}
+            cover={<img alt='image' src={item.urlToImage}/>}
+            >
+                <Meta title ={item.title} description={item.content}/>
+                <a href={item.url} target='_blank' rel='noopener noreferrer'>
+                <Button type='primary' style={{marginTop:'10px'}}>Read More </Button>
+                </a>
+            </Card>
+        )
+       } )}
         
         </div>
-        </>
     )
 }
 export default NewsStories;
